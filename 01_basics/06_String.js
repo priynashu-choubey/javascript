929f3cf5cbaf1ea6c620907328bbb9a2b6c2f240
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-16 23:33:14 +0000

    01_basics/06_String.js

--- 01_basics/06_String.js
+++ 01_basics/06_String.js
@@ -1,2 +1,8 @@
 const gameName= new String ('Hitesh Sir');
 console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('i'));
