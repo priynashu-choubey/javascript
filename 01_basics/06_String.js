a640183bdb4e56070cc0890d17aea2beb1e31cae
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-17 00:11:09 +0000

    replace added

--- 01_basics/06_String.js
+++ 01_basics/06_String.js
@@ -10,3 +10,7 @@ console.log(gameName.charCodeAt('4'));
 const newString=gameName.substring(0,4);
 console.log(newString);
 console.log(gameName.substring(2,6));
+const newName=gameName.slice(-8,4)
+console.log(newName);
+const url="https://Priyanshu%20choubey.in"
+console.log(url.replace('%20','_'));
