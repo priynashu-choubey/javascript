f3ee0668bd757c85cde1f965d3066bac7fd81346
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-20 11:15:09 +0000

    Push

--- 01_basics/07_NumsAndMaths.js
+++ 01_basics/07_NumsAndMaths.js
@@ -1,12 +1,11 @@
-// const score=400;
-// const balance=new Number(100);
-// console.log(balance);
-// console.log(balance.toString().length);
-// console.log(balance.toString(2));
-// const otherNum =23.4541;
-<<<<<<< HEAD
-// console.log(otherNum.toPrecision(2));
-// console.log(otherNum.toFixed(2).localeSting('en-IN'));
+const score=400;
+const balance=new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toString(2));
+const otherNum =23.4541;
+console.log(otherNum.toPrecision(2));
+console.log(otherNum.toFixed(2).localeSting('en-IN'));
 
 //Maths Library
 
@@ -14,14 +13,13 @@
 //console.log(Math.max(45,54545,24))
 console.log(Math.floor((Math.random()*10)+1));
 
-/*+++++++++++++ WE HAVE A TRICK   ++++++++++*/
+
+
+
+/*+++++++++++++++++++++++++++ WE HAVE A TRICK   ++++++++++++++++++++++++++++++++++++*/
 const min=10;
 const max=20;
-<<<<<<< HEAD
 console.log(Math.floor(Math.random()*(max-min+1))+min);
 
-//This is a trick to produce a random number with a given range
-=======
-console.log(Math.floor(Math.random()*(max-min*10))+min);
->>>>>>> 9292c63 (Final Push)
+// this is a trick to produce random number with a given range
 
