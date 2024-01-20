9de279851e272f30b4b573fc93ce5371ab4515f8
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-20 16:32:10 +0530

    Update 07_NumsAndMaths.js

--- 01_basics/07_NumsAndMaths.js
+++ 01_basics/07_NumsAndMaths.js
@@ -1,5 +1,22 @@
-const score=400;
-const balance=new Number(100);
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toString(2));
+// const score=400;
+// const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toString(2));
+// const otherNum =23.4541;
+// console.log(otherNum.toPrecision(2));
+// console.log(otherNum.toFixed(2).localeSting('en-IN'));
+
+//Maths Library
+
+//1. Math.max
+//console.log(Math.max(45,54545,24))
+console.log(Math.floor((Math.random()*10)+1));
+
+/*+++++++++++++ WE HAVE A TRICK   ++++++++++*/
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+//This is a trick to produce a random number with a given range
+
