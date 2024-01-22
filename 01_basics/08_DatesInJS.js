14e0e0b5757ad8f946fda7d76cf96d1a579c7b2b
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-22 11:54:33 +0000

    Dates Compete

--- 01_basics/08_DatesInJS.js
+++ 01_basics/08_DatesInJS.js
@@ -1,3 +1,28 @@
+//++++++++++++++++++ DATES++++++++++++
+//      1
+
 let myDate = new Date();
 console.log(myDate);
-console.log(myDate.toString());
+console.log(myDate.toString('IN'));
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof (myDate));
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+//      2
+let myCreatedDate=new Date(2023,0,22);
+console.log("RAM RAJYA ON  "+myCreatedDate.toTimeString());
+console.log("Ram Rajya on "+ myCreatedDate.toDateString());
+console.log();
+console.log();
+console.log();
+console.log();
+
+//      3
+//(In this format the month starts from 01 for january unlike other two declaration type)        
+let myCreatedDate3=new Date("2023-012-22");  
+console.log(myCreatedDate3);
