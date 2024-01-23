0c124c80aff84ac0c9d3a9f80cc250da4ddf8c2d
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-23 16:26:03 +0000

    Comitted

--- 02_basics/01_Arrays.js
+++ 02_basics/01_Arrays.js
@@ -1,4 +1,38 @@
-const myArray=[0,1,2,3,4,5];
-console.log(myArray[2]);
-myArray.push(6,7,8);
-console.log(myArray);
+// --------------------------01--------------------
+
+ const myArray=[0,1,2,3,4,5];
+// console.log(myArray[2]);
+// myArray.push(6,7,8);
+// console.log("A: ",myArray);
+// console.log(myArray.unshift(0));
+// console.log("B ",myArray);
+// console.log("C ",myArray.unshift(2));
+// console.log("D ",myArray);
+// myArray.shift();
+// myArray.shift();
+// myArray.shift();
+// console.log("E",myArray); 
+// myArray.push(10,11,12);
+// console.log("F ",myArray);
+//
+//********************************************************** */
+//  Merging an Array
+// const ar1=["Rolando","Messi"]
+// const ae2=["Bhutia", "Chetri"]
+// ar1.push(...ae2)
+// console.log("A ",ar1);
+
+/********************************************************/
+// console.log(myArray.includes(45));
+// console.log(myArray.reverse());
+// const newArr=myArray.join();
+// console.log("A:",newArr);
+
+//------------------SLICE-----------------
+// console.log("A:",myArray);
+// const arr3=myArray.slice(1,4);
+// console.log(arr3);
+// console.log("B:",myArray);
+
+//-----------------SPLICE------------------
+const arr4=myArray.splice(0,1,"Mango")
