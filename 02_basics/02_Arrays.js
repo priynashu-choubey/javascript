2fec83955724e0fa525f2e3b920d47dfa563bde7
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-01-24 04:02:30 +0000

    last array push

--- 02_basics/02_Arrays.js
+++ 02_basics/02_Arrays.js
@@ -1,3 +1,4 @@
+//******************   PUSH FUNCTION ****************************/
  const marvel_h=["Thor","Ironman","Captain","Spidy"];
 // console.log("Marvel:",marvel_h);
  const dc_h=["Superman","Flash","Batman"];
@@ -8,13 +9,38 @@
 // console.log("allHeros: ",allHeros);
 
 
-//*****************************************************************/
+//***************************NOTES*********************************/
 //Concat and push has a diference 
 //Concat adds array and gives the outpu whereas the 
 //Push func gives the length of Array 
+//****************************************************************/
 
 
+//***************Spread operator (...) ***************************/
+// const assembledAVG=[...marvel_h,...dc_h]
+// console.log(assembledAVG);
 
-//||||||||||||||||||||| spreda operator (...) |||||||||||||||||||||||||||
-const assembledAVG=[...marvel_h,...dc_h]
-console.log(assembledAVG);
+
+// //******** FLAT FUNCTION   ************************************/
+// const anotherArray=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// console.log("anotherArray Brfore: ",anotherArray);
+// const realArr=anotherArray.flat(Infinity) 
+// // the number inside flat function describes the level of flatting
+// console.log("another Array After: ",anotherArray);
+// console.log("This is reakArr: ",realArr);
+
+//****************** isArray Function *****************************/
+console.log(Array.isArray("Prince"));
+
+// Used to sk questions
+
+//*************** Array.from ************************************/
+console.log(Array.from("Prince"));
+console.log(Array.from({name:"nitesh"}));
+// Used to make string as a separate Array
+
+//*********************    Array.of Method      *******************/
+let s1=100,s2=200,s3=300,s4=400;
+const arrof=Array.of(s1,s2,s3,s4);
+console.log("Array of ",arrof);
+console.log(Array.of(s1,s2,s3,s4)) ;
