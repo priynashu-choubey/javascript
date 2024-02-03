9dec58c9e3a961ae671f9af8d7a079ed37f76d19
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-02-03 11:24:37 +0000

    Final Functions

---
+++ 03_basics/03_Scopes.js
@@ -0,0 +1,34 @@
+function one() {
+    const username="Priyanshu"
+
+    function two () {
+        const website="youtube"
+        console.log(username);
+    }
+   // console.log(website);
+    // two()
+}
+one()
+if(true){
+    const username="Priyanshu"
+    if(username==="Prianshu"){
+        const website="Youutube"
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+/*++++++++++++++++++++INTERsting++++++++++++++++++++++++++++++++++*/
+console.log(addone(5));
+function addone(num) 
+{
+return num+1;    
+}
+console.log(addone(5));
+const addTwo=function(num2){
+    return num2+2;
+}
+console.log(addTwo(6));
+
+//this concept is called hoisting
