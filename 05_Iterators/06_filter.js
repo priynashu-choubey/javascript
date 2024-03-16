16b93bf5eab2278031e619058af78f66c76bf801
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-03-16 15:38:18 +0000

    Filter is are almost Covred

--- 05_Iterators/06_filter.js
+++ 05_Iterators/06_filter.js
@@ -8,6 +8,50 @@ const values=coding.forEach(
 )
 console.log(values); 
 */
-const myNums=[1,2,3,4,5,6,7,8,9,10];
+
+// if we are using Scope that is {} then 
+/* we have to type return as shown in 2nd
+   function else we are  good as in Ex 1
+
+   const myNums=[1,2,3,4,5,6,7,8,9,10];
 const newNum=myNums.filter((num) =>  num>4)
 console.log(newNum);
+*/
+
+const myNums=[1,2,3,4,5,6,7,8,9,10];
+// Method 01
+/* const newNum=myNums.filter((num) => 
+{ 
+ return    num>4
+})
+console.log(newNum); */
+// 02 Method 02
+/* const newNums = []
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+} )
+
+console.log(newNums); 
+*/
+
+// Example of Filter uses
+/* const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks); */
