07f9190482d5c0ed29e40252066f4cda3c8de3f6
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-03-16 05:47:52 +0000

    Filer Added

---
+++ 05_Iterators/06_filter.js
@@ -0,0 +1,13 @@
+// For each Always Returns Undefined
+/* const coding=["JS","Ruby","Java","Python","CPP"];
+const values=coding.forEach(
+    (item)=>{
+        //console.log(item);
+        //return item
+    }
+)
+console.log(values); 
+*/
+const myNums=[1,2,3,4,5,6,7,8,9,10];
+const newNum=myNums.filter((num) =>  num>4)
+console.log(newNum);
