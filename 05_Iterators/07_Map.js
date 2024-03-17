f362b1a12398e1b5fa4dfb83c2f4ae713177f593
Priyanshu Choubey <107321290+Priyanshu-ghub@users.noreply.github.com>
2024-03-17 07:13:21 +0000

    chaining method added

--- 05_Iterators/07_Map.js
+++ 05_Iterators/07_Map.js
@@ -1,6 +1,16 @@
 // MAP IS A CALLBACL FUNCTION
 const myNums=[1,2,3,4,5,6,7,8,9,10];
-const nums=myNums.filter((num2)=>{
+/*const nums=myNums.map((num2)=>{
     return myNums+10;
 })
-console.log(nums);
+console.log(nums); 
+*/
+// CHAINIG METHOD
+const newNums=myNums.map((num)=>
+{
+    return num*10
+})
+.map((num)=>{
+return num+1;
+})
+.filter((num)=> num>=40)
